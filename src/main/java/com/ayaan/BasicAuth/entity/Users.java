1212850e5b33c144234fae0769c90182c039b0dd
Ayaan Pathan <ayaanpathanra@gmail.com>
2026-06-17 14:19:54 +0530

    "Created User Entity"

---
+++ src/main/java/com/ayaan/BasicAuth/entity/Users.java
@@ -0,0 +1,30 @@
+package com.ayaan.BasicAuth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String role;
+
+}
